ef631bff3348c4c86af99a0db91657c5bee70c7b
Lucas Veron <62112162+lucaseveron@users.noreply.github.com>
2025-10-18 12:19:14 -0300

    Update payload name to include formatted time

--- sonar_to_dojo.py
+++ sonar_to_dojo.py
@@ -75,9 +75,10 @@ def create_dojo_engagement(product_id, name):
     today = datetime.now().date()
     next_week = today + timedelta(days=7)
     fecha = datetime.now().time()
+    formateo_fecha = fecha.strftime("%H:%M:%S")
 
     payload = {
-        "name": f"Import Sonarcloud - {fecha} ",
+        "name": f"Import Sonarcloud - {formateo_fecha} ",
         "product": product_id,
         "status": "In Progress",
         "target_start": str(today),
